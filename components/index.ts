cf93decfee9ecc8a7b87de6251dc0f8e52afb08e
Dawid Rynkal <rynkal.dawid@gmail.com>
2023-11-10 17:16:04 +0100

    cars list (rapid api)

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,20 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
